44c2fd27b2353f2324da5bc90293b173e3bd0225
Chiang Fong Lee <myself@cflee.net>
2017-10-21 22:40:51 +0800

    Switch ESC to GRAVE_ESC

--- keyboards/planck/keymaps/cflee/keymap.c
+++ keyboards/planck/keymaps/cflee/keymap.c
@@ -28,13 +28,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  *
- * Cmd = GUI (Winkey)   without OS swaps and with MAGIC_UNSWAP_ALT_GUI/AG_NORM
- * Opt = Alt
+ * Modifiers, without OS swaps and with AG_NORM (MAGIC_UNSWAP_ALT_GUI)
+ *   Cmd = GUI (Winkey)
+ *   Opt = Alt
  *
+ * Esc is GRAVE_ESC
+ *   GUI-Esc is Cmd-`
+ *   Shift-Esc is Shift-`, aka ~
  */
 [_QWERTY] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
   {_______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
