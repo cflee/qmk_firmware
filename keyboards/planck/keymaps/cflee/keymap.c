9bf7ea6bbf328a51018f6045723b1a194af18c9a
Chiang Fong Lee <myself@cflee.net>
2018-09-09 14:17:52 +0800

    Remove enter from Raise-Shift key

--- keyboards/planck/keymaps/cflee/keymap.c
+++ keyboards/planck/keymaps/cflee/keymap.c
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  |   4  |   5  |   6  |   +  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |Enter |   7  |   8  |   9  |   -  |  F11 |  F12 |      | Home |Pg Dn |Pg Up | End  |
+ * |      |   7  |   8  |   9  |   -  |  F11 |  F12 |      | Home |Pg Dn |Pg Up | End  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |    Bksp     |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = {
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
   {KC_DEL,  KC_4,    KC_5,    KC_6,    KC_PLUS, KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
-  {KC_ENT,  KC_7,    KC_8,    KC_9,    KC_MINS, KC_F11,  KC_F12,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END },
+  {_______, KC_7,    KC_8,    KC_9,    KC_MINS, KC_F11,  KC_F12,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END },
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
 },
 
